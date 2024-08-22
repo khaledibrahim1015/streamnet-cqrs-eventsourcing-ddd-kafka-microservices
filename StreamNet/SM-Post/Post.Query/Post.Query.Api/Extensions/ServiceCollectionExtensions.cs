6e81ff411d9595b63c17cbe5e6e58930d49e29fd
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-22 05:31:31 +0300

    add DatabaseContext and DatabaseContextFactory Register all in ServiceCollectionExtensions

---
+++ StreamNet/SM-Post/Post.Query/Post.Query.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Infrastructure.DataAccess;
+
+namespace Post.Query.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRequiredService(this IServiceCollection services, IConfiguration configuration)
+        {
+            Action<DbContextOptionsBuilder> configureDbContext = o => o.UseLazyLoadingProxies().UseSqlServer(configuration.GetConnectionString("SqlServer"));
+            services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
+
+            //services.AddDbContext<DatabaseContext>(configureDbContext);
+            services.AddScoped(serviceProvider =>
+            {
+                var databaseCtxFactory = serviceProvider.GetRequiredService<DatabaseContextFactory>();
+                return databaseCtxFactory.CreateDbContext();
+            });
+
+
+
+            return services;
+        }
+    }
+}
---
+++ StreamNet/SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Post.Query.Infrastructure.DataAccess;
+
+public class DatabaseContextFactory
+{
+    private readonly Action<DbContextOptionsBuilder> _configureDbContext;
+
+    public DatabaseContextFactory(Action<DbContextOptionsBuilder> configureDbContext)
+    {
+        _configureDbContext = configureDbContext;
+    }
+
+    public DatabaseContext CreateDbContext()
+    {
+        DbContextOptionsBuilder<DatabaseContext> optionBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+        //  invoke method 
+        _configureDbContext(optionBuilder);
+
+        return new DatabaseContext(optionBuilder.Options);
+    }
+}
