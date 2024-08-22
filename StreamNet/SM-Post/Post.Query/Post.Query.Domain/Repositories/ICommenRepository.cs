3640a861230f5e428fc0d3f82ad7f6e4f0ee21aa
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-22 04:51:47 +0300

    add domain model and Repositories for Post.Query

---
+++ StreamNet/SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommenRepository.cs
@@ -0,0 +1,13 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories;
+
+public interface ICommenRepository
+{
+
+    Task CreateCommentAsync(CommentEntity comment);
+    Task UpdateCommentAsync(CommentEntity comment);
+    Task DeleteCommentAsync(Guid commentId);
+    Task<CommentEntity> GetByIdAsync(Guid commentId);
+
+}
---
+++ StreamNet/SM-Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories;
+
+public interface IPostRepository
+{
+    Task CreatePostAsync(PostEntity post);
+    Task UpdatePostAsync(PostEntity post);
+    Task DeletePostAsync(Guid postId);
+
+
+    Task<PostEntity> GetPostByIdAsync(Guid postId);
+    Task<List<PostEntity>> ListAllAsync();
+
+    Task<List<PostEntity>> ListByAuthorAsync(string author);
+    Task<List<PostEntity>> ListByLikesAsync(int numberOfLikes);
+
+    Task<List<PostEntity>> ListWithCommentsAsync();
+
+
+}
